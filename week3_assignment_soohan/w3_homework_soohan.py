67331a34a1ef09141fae1fc2f034a75d05f8f58d
Soohan <soohancho@icloud.com>
2020-10-14 19:55:43 +0900

    w3_salty_assignment
    몇개 수정했습니다.

--- week3_assignment_soohan/w3_homework_soohan.py
+++ week3_assignment_soohan/w3_homework_soohan.py
@@ -1,28 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
-#DB
+
+# DB
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
-#REQUEST
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# REQUEST
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 chart_date = 20201010
-data = requests.get(f"https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd={chart_date}",headers=headers)
+data = requests.get(f"https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd={chart_date}", headers=headers)
 
-#PROCESS
+# PROCESS
 soup = BeautifulSoup(data.text, 'html.parser')
 songs = soup.select("#body-content > div.newest-list > div > table > tbody > tr")
 
-
 for song in songs:
     rank = song.select_one(".number").text[0:2].strip()
-    artist = song.select_one(".artist").text.strip()
     title = song.select_one(".title").text.strip()
-    db_data={
-        "rank":rank,"artist":artist,"title":title
+    artist = song.select_one(".artist").text.strip()
+    db_data = {
+        "rank": rank, "title": title, "artist": artist,
     }
     db.genie.insert_one(db_data)
 
+# CONFIRM
 all_data = list(db.genie.find({}))
 for item in all_data:
-    print(item["artist"])
+    rank = item["rank"]
+    title = item["title"]
+    artist = item["artist"]
+    print(rank, title, artist)
